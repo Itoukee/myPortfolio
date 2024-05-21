edd55947b6eba9f9d2f2c9e5a36534f50b8a1581
Nino <nino.garo@gmail.com>
2024-05-22 01:05:11 +0200

    some fun css

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,50 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import DeloreanScene from "./components/Delorean";
 import TextGradient from "./components/design/TextGradient";
 
 const App = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 769px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <div className="w-screen h-screen overflow-auto">
-      <h1 className="w-fullflex flex-wrap sticky  font-bold space-x-4  md:w-1/2">
-        <TextGradient
-          text={"Nino"}
-          className="font-bold"
-          from="from-violet-700"
-        />
-        <span>GARO</span>
-      </h1>
-      <DeloreanScene />
+      <div className="w-full h-full flex">
+        <div className="w-full bg-slate-300 md:w-1/3 ">
+          <h1 className="flex flex-wrap m-4 sticky font-bold space-x-4">
+            <TextGradient text={"Nino"} from="from-violet-700" />
+            <span>GARO</span>
+          </h1>
+        </div>
+
+        {isMobile ? (
+          <></>
+        ) : (
+          <div className="bg-red-300 md:w-full">
+            <DeloreanScene />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/Delorean.tsx
+++ src/components/Delorean.tsx
@@ -1,6 +1,5 @@
 import { Environment, OrbitControls, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { useEffect, useState } from "react";
 
 const Delorean = () => {
   const gltf = useGLTF("./src/assets/3D/scene.gltf");
@@ -17,52 +16,23 @@ const Delorean = () => {
 };
 
 const DeloreanScene = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
-    setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
-    const handleMediaQueryChange = (event: MediaQueryListEvent) => {
-      setIsMobile(event.matches);
-    };
-
-    // Add the callback function as a listener for changes to the media query
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
-
   return (
-    <>
-      {isMobile ? (
-        <></>
-      ) : (
-        <Canvas
-          shadows
-          frameloop="demand"
-          dpr={[1, 2]}
-          camera={{ position: [5, 1, 25], fov: 40 }}
-          gl={{ preserveDrawingBuffer: true }}
-        >
-          <OrbitControls
-            enableZoom={false}
-            enablePan={false}
-            maxPolarAngle={Math.PI / 2}
-            minPolarAngle={Math.PI / 2}
-          />
-          <Delorean />
-          <Environment preset="dawn" />
-        </Canvas>
-      )}
-    </>
+    <Canvas
+      shadows
+      frameloop="demand"
+      dpr={[1, 2]}
+      camera={{ position: [5, 1, 25], fov: 40 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <OrbitControls
+        enableZoom={false}
+        enablePan={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+      />
+      <Delorean />
+      <Environment preset="dawn" />
+    </Canvas>
   );
 };
 
